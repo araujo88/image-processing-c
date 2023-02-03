7bcedd676083bea536dbe1977bfefe8d498701b6
Leonardo Araujo <leonardo.aa88@gmail.com>
2023-02-03 18:49:17 -0300

    fix: ffmpeg script

--- noise/generate_video.sh
+++ noise/generate_video.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ffmpeg -i noise-%d.bmp -r 10 -c:v libx264 -preset slow -crf 10 output.mp4
+ffmpeg -i noise-%d.bmp -r 10 -c:v libx264 -preset fast -crf 18 -pix_fmt yuv420p output.mp4
